cfd95613e2c3017903fdbb9a011f048cb897a1d9
Marga <margalppalacios@gmail.com>
2022-07-25 12:18:12 +0200

    ejercicios sencillos de mysql

--- SQL/query_sakila/querys_sakila.sql
+++ SQL/query_sakila/querys_sakila.sql
@@ -130,3 +130,52 @@ SELECT
 FROM 
     actor 
 WHERE upper(first_name) LIKE '__B%';
+
+
+-- Direcciones de california que tengan ‘274’ en el número de teléfono
+SELECT
+	*
+FROM
+	sakila.address
+WHERE district = 'California'
+AND phone LIKE '%274%';
+
+
+-- Películas ‘Épicas’ (Epic) o ‘Brillantes’ (brilliant) que duren más de 180 minutos
+
+SELECT
+	*
+FROM
+	sakila.film
+WHERE (description LIKE '%Epic%' 
+OR description LIKE '%brilliant%') 
+AND length >180;
+
+
+-- Películas que duren entre 100 y 120 minutos o entre 50 y 70 minutos
+
+SELECT
+	*
+FROM
+	sakila.film
+WHERE length between 100 AND 120 
+OR length between 50 AND 70;
+
+
+-- Películas que cuesten 0.99, 2.99 y tengan un rating ‘g’ o ‘r’ y que hablen de cocodrilos (cocodrile)
+
+SELECT
+	*
+FROM
+	sakila.film
+WHERE description LIKE '%crocodile%' AND rental_rate IN ( 0.99, 2.99) 
+AND rating IN ( 'R', 'G');
+
+
+-- Direcciones de ontario o de punjab o que su código postal acabe en 5 o que su teléfono acabe en 5
+
+SELECT * FROM sakila.address
+WHERE district='ontario' 
+    OR district='punjab'
+    OR postal_code like '%5' 
+    OR phone like '%5';
