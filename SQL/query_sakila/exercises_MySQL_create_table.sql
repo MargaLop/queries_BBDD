f69486c9c43f1cffd652e247ffc01c64583649f1
Marga <margalppalacios@gmail.com>
2022-07-26 12:49:36 +0200

    4 EJERCICIOS

--- SQL/query_sakila/exercises_MySQL_create_table.sql
+++ SQL/query_sakila/exercises_MySQL_create_table.sql
@@ -20,3 +20,39 @@ region_id INT(11)
 
 CREATE TABLE IF NOT EXISTS dup_countries
 LIKE countries;
+
+
+-- Write a SQL statement to create a duplicate copy of countries table including structure and data by name dup_countries
+
+CREATE TABLE IF NOT EXISTS dup_countries
+AS SELECT * FROM countries;
+
+
+-- Write a SQL statement to create a table countries set a constraint NULL.
+
+CREATE TABLE IF NOT EXISTS countries ( 
+country_id INT(11) NOT NULL,
+country_name VARCHAR(45) NOT NULL,
+region_id INT(11) NOT NULL
+);
+
+
+-- Write a SQL statement to create a table named jobs including columns job_id, job_title, min_salary, max_salary and check whether the max_salary amount exceeding the upper limit 25000.
+CREATE TABLE IF NOT EXISTS jobs(
+job_id INT(11) NOT NULL,
+job_title varchar(45) NOT NULL, 
+min_salary INT(11), 
+max_salary INT(11)
+CHECK(max_salary<=25000)
+);
+
+
+-- Write a SQL statement to create a table named countries including columns country_id, country_name and region_id and make sure that no countries except Italy, India and China will be entered in the table.
+
+
+CREATE TABLE IF NOT EXISTS countries ( 
+country_id INT(11) NOT NULL,
+country_name VARCHAR(45) NOT NULL,
+region_id INT(11) NOT NULL
+CHECK(country_name IN('Italy','India','China')) 
+);
